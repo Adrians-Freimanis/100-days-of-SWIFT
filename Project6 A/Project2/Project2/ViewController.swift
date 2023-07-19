c61ac0d402b5f6eeb82797ecc3b9f4ee38793a71
Adrians-Freimanis <adrians.freimanis@accenture.com>
2023-07-19 16:27:07 +0300

    Project 6 after challenge

---
+++ Project6 A/Project2/Project2/ViewController.swift
@@ -0,0 +1,114 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by adrians.freimanis on 18/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var ButtonOne: UIButton!
+    @IBOutlet var buttonTwo: UIButton!
+    @IBOutlet var ButtonThree: UIButton!
+    
+    var contries = [String]()
+    var score = 0
+    var correctAnswer = 0
+    var buttonTappedCount = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        contries += ["estonia", "france", "germany", "ireland", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        ButtonOne.layer.borderWidth = 1
+        buttonTwo.layer.borderWidth = 1
+        ButtonThree.layer.borderWidth = 1
+        
+        ButtonOne.layer.borderColor = UIColor.lightGray.cgColor
+        buttonTwo.layer.borderColor = UIColor.lightGray.cgColor
+        ButtonThree.layer.borderColor = UIColor.lightGray.cgColor
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(infoTapped))
+        
+        
+        
+        
+        askQuestion()
+    }
+    
+    func resetGame(action: UIAlertAction! = nil){
+        score = 0
+        buttonTappedCount = 0
+        askQuestion()
+    }
+    
+    func askQuestion(action: UIAlertAction! = nil){
+        contries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
+        ButtonOne.setImage(UIImage(named: contries[0]), for: .normal)
+        buttonTwo.setImage(UIImage(named: contries[1]), for: .normal)
+        ButtonThree.setImage(UIImage(named: contries[2]), for: .normal)
+        
+        
+        let contry = contries[correctAnswer].uppercased()
+        title = "\(contry)(\(buttonTappedCount))"
+        
+        let finalAC = UIAlertController(title: "Result", message: "You answered 10 questions and got \(score) correct!", preferredStyle: .alert)
+        finalAC.addAction(UIAlertAction(title: "reset", style: .default, handler: resetGame))
+        
+        if(buttonTappedCount == 10){
+            present(finalAC, animated: true)
+        }
+    }
+    
+   
+    
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        buttonTappedCount += 1
+        
+        var title: String
+        
+        if sender.tag == correctAnswer{
+            title = "Correct"
+            score += 1
+        } else{
+            title = "Wrong"
+            score -= 1
+        }
+        if sender.tag == correctAnswer{
+            let ac = UIAlertController(title: title, message: "Your score is \(score). You got the answer correct", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+            
+            present(ac, animated: true)
+        }else{
+            let ac = UIAlertController(title: title, message: "Your score is \(score).You got the answer wrong", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+            present(ac, animated: true)
+        }
+    }
+    
+    
+    @objc func infoTapped(){
+        
+        let ac = UIAlertController(title: title, message: "Your current score is \(score)", preferredStyle: .actionSheet)
+        
+        ac.addAction(UIAlertAction(title: "Back", style: .default, handler: { [weak self] _ in
+               self?.dismiss(animated: true, completion: nil)
+           }))
+        
+        
+        present(ac,animated:true)
+        
+    }
+    
+    
+    
+
+}
+
