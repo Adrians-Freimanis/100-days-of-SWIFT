d0f665686b04abf09ead6a9c778b0dccfd636bfc
Adrians-Freimanis <adrians.freimanis@accenture.com>
2023-07-25 16:35:49 +0300

    commit

---
+++ Project12 B/Project10/Project10/Person.swift
@@ -0,0 +1,20 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by adrians.freimanis on 24/07/2023.
+//
+
+import UIKit
+
+class Person: NSObject,Codable {
+
+    
+    var name: String
+    var image: String
+    
+    init(name: String, image: String){
+        self.name = name
+        self.image = image
+    }
+}
---
+++ Project12 B/Project10/Project10/PersonCell.swift
@@ -0,0 +1,17 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by adrians.freimanis on 24/07/2023.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var imageLabel: UILabel!
+    
+    
+}
